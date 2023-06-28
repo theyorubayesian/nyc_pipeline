e7bffb5917e3802d22b4b49354c1846de0e64625
Seun-Ajayi <youoluwaseunaajayi@student.oauife.edu.ng>
2023-06-28 05:06:48 +0100

    added new cleaning mechanism

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,7 +32,10 @@ def go(args):
     idx = df['price'].between(float(args.min_price), float(args.max_price))
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
-    # TODO: add code to fix the issue happened when testing the model
+    
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     
 
     # Save the cleaned data
